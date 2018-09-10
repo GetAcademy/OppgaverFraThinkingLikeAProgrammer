9495c8e43738d46700f8a242af8e263e079784a3
Terje Kolderup <terje@kolderup.net>
2018-09-10 10:45:09 +0200

    x

---
+++ OppgaverFraThinkingLikeAProgrammer/Exercise2-1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppgaverFraThinkingLikeAProgrammer
+{
+    class Exercise2_1
+    {
+    }
+}
